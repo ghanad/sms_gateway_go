dd22cbca45d74c53795169a2e6ff8ff148601780
Aghajani <aghajani.ali@gmail.com>
2025-08-21 00:25:39 +0330

    update

--- sms-gateway-project/frontend/entrypoint.sh
+++ sms-gateway-project/frontend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Replace placeholder with actual API URL
-sed -i "s|\$VITE_API_BASE_URL|$VITE_API_BASE_URL|g" /etc/nginx/conf.d/default.conf
+envsubst '$$VITE_API_BASE_URL' < /etc/nginx/conf.d/default.conf > /tmp/default.conf && mv /tmp/default.conf /etc/nginx/conf.d/default.conf
 
 exec "$@"
